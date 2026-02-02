e5090a32877144f42ed6210fb0af7e904a15d53c
vedashree-05 <163523010+vedashree-05@users.noreply.github.com>
2026-02-02 11:55:15 +0530

    Add files via upload

--- lab1.py
+++ lab1.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import numpy as np
+import numpy as np 
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.datasets import fetch_california_housing 
@@ -9,31 +9,31 @@ numerical_features=housing_df.select_dtypes(include=[np.number])
 numerical_features.head()
 
 plt.figure(figsize=(15,10))
-for i,features in enumerate (numerical_features.columns):
+for i , feature in enumerate(numerical_features.columns):
     plt.subplot(3,3,i+1)
     sns.histplot(data=housing_df[feature],kde=True,bins=45,color='blue')
-    plt.title(f'Histogram of {features}')
+    plt.title(f'histogram of{feature}')
 plt.tight_layout()
 plt.show()
 
 plt.figure(figsize=(15,10))
-for i,features in enumerate (numerical_features.columns):
+for i , feature in enumerate(numerical_features.columns):
     plt.subplot(3,3,i+1)
-    sns.boxplot(data=housing_df[feature],kde=True,bins=45,color='blue')
-    plt.title(f'Boxplot of {features}')
+    sns.boxplot(data=housing_df[feature],color='orange')
+    plt.title(f'box Plot of{feature}')
 plt.tight_layout()
 plt.show()
 
-print("Description of outliers")
-outliers_summary={ }
-for feature in numerical_features.columns:
-    Q1=housing_df[feature].quantile(0.25)
-    Q3=housing_df[feature].quantile(0.75)
-    IQR=Q3-Q1
-    lower_bound=Q1-1.5*IQR
-    upper_bound=Q3+1.5*IQR
-    outliers=housing_df[(housing_df[feature]<lower_bound)|(housing_df[feature]>upper_bound)]
-    outliers_summary[feature]=len(outliers)
+
+print("Description of Outliers")
+outliers_summary={}
+for i , feature in enumerate(numerical_features.columns):
+        Q1=housing_df[feature].quantile(0.25)
+        Q3=housing_df[feature].quantile(0.75)
+        IQR=Q3-Q1
+        lb=Q1-1.5*IQR
+        ub=Q3+1.5*IQR
+        outliers=housing_df[(housing_df[feature]<lb)|(housing_df[feature]>ub)]
+        outliers_summary[feature]=len(outliers)
 print(outliers_summary)
 print(housing_df.describe())
-
